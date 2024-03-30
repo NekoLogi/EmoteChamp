82330bf9f967f257ef15ae570ab6bf93099c5b92
NekoLogi <nekologi@gmail.com>
2024-03-30 18:25:58 +0100

    Modified -> Main.java, if opening URI in browser fails, URI gets copied to clipboard.

--- src/main/java/com/gmail/nekologi/Main.java
+++ src/main/java/com/gmail/nekologi/Main.java
@@ -1,7 +1,10 @@
 package com.gmail.nekologi;
 
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.File;
+import java.net.URI;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,8 +12,22 @@ public class Main {
         try {
             Html html = new Html(new File(".").getCanonicalPath());
             filePath = html.generate();
+            URI uri = new File(filePath).toURI();
 
-            Desktop.getDesktop().browse(new File(filePath).toURI());
+            Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+            if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE))
+                desktop.browse(uri);
+            else {
+                StringSelection selection = new StringSelection(uri.toString());
+                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clipboard.setContents(selection, selection);
+                Thread.sleep(1000);
+
+                System.out.println("EmoteChamp was not able to open the html page " +
+                        "and added the URI now to the clipboard.");
+                System.out.println(selection);
+                System.out.println(uri.toString());
+            }
         } catch (Exception err) {
             System.out.println(err.getMessage());
             throw new RuntimeException(err);
